174d8de9032c0e7dad3435dfe974f17c2caa3bdf
Alina-cyber1 <khasanovaalina34@gmail.com>
2026-03-09 21:07:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import numpy as np
 import io
 import matplotlib.pyplot as plt
 import os
-
+print("🚀 app.py начал выполняться")
 # Импортируем наш загрузчик данных
 from data_loader import load_domain_data
 
